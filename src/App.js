8310b35c1d95bf228b238ad70e1da3c26ee94485
William Talley <williamtalley51@gmail.com>
2023-11-16 10:15:45 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import JSONDATA from './MOCK_DATA.json'
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState('')
+  return <div className="App">
+    <input type="text" placeholder="Search....." onChange={event => {setSearchTerm(event.target.value);}} />
+    {JSONDATA.filter((val)=> {
+      if (searchTerm == ""){
+        return val
+      } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase()) || val.last_name.toLowerCase().includes(searchTerm.toLowerCase())){
+        return val
+      }
+    }).map((val, key)=> {
+      return (
+      <div className="user" key={key}>
+        <p>{val.first_name} {val.last_name}</p>
+        </div>
+      );
+    })}
+  </div>
+}
+
+export default App;
